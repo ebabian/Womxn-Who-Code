810413d7529a932cb87662317afa0f93ed423a22
Elektra Babian <babianelektra@gmail.com>
2020-06-20 11:35:31 -0400

    added to the server

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const app = express()
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
+
+
+//use .json()
+app.use(express.json());
+
+// public folder is static and public
+app.use(express.static('public'))
 
 
 // http://localhost:3000/wmxn
+
+//controller
+const wmxnController = require('./controllers/routes.js')
+app.use('/wmxn', wmxnController)
+
+
+mongoose.connect('mongodb://localhost:27017/wmxn',
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 
 app.get('/', (req, res) => {
